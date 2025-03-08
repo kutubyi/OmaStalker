f92b30673681dd5c497bad0915d4a1d18c9bc28d
kutubyi <liyinuo.00@outlook.com>
2025-03-08 21:06:54 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-RESERVATION_URL = "https://omakase.in/ja/r/xxxxxxxx"  # Change this to an actual URL
+RESERVATION_URL = "https://omakase.in/ja/r/fa131638"  # Sugita, but change to any omakase.in URL you want to monitor
